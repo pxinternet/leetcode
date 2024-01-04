34b2cb7a023c93d5fdc6bdb3578a467471483e94
lv <nechina2020@gmail.com>
2024-01-04 22:46:14 +0800

    206

--- src/LC206.java
+++ src/LC206.java
@@ -1,5 +1,18 @@
 public class LC206 {
     public ListNode reverseList(ListNode head) {
+        if (head == null) return head;
+
+        ListNode pre = null;
+        ListNode curr = head;
+
+        while(curr !=null) {
+            curr = head.next;
+            head.next = pre;
+            pre = head;
+            head = curr;
+        }
+
+        return pre;
 
     }
 }
