6d5d8fc9b7793b56adcb6ada6e86968df5091d19
lv <nechina2020@gmail.com>
2024-01-10 15:54:00 +0800

    init

--- src/LC91numDecoding.java
+++ src/LC91numDecoding.java
@@ -29,4 +29,37 @@ public class LC91numDecoding {
         }
         return dp[n];
     }
+
+
+    public int numDecodingsMemoryImprove(String s) {
+
+        int n = s.length();
+
+        //i -2
+        int f2 = 0;
+
+        //i -1
+        int f1 = 1;
+
+        //final
+        int res = 0;
+
+        //0个字符有一种编码方式，返回0
+
+        for (int i = 1; i <= n; i++) {
+            //这步不能忘记，res每次进入循环都要初始化一下
+            res = 0;
+            int a = s.charAt(i-1) - '0';
+            if (a >=1 && a <= 9) res += f1;
+
+
+            if (i > 1) {
+                int b = (s.charAt(i - 2) - '0') * 10 + a;
+                if (b >= 10 && b <= 26) res += f2;
+            }
+            f2 = f1;
+            f1 = res;
+        }
+        return res;
+    }
 }
