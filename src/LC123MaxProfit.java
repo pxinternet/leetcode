f3e67ae0eeca47fb72c26039f3d5197160e53ddb
lv <nechina2020@gmail.com>
2024-01-10 15:38:04 +0800

    dp

--- src/LC123MaxProfit.java
+++ src/LC123MaxProfit.java
@@ -29,6 +29,7 @@ public class LC123MaxProfit {
             sell1 = Math.max(sell1, buy1 +prices[i]);
             buy2 = Math.max(buy2, sell1 - prices[i]);
             sell2 = Math.max(sell2, buy2 + prices[i]);
+            System.out.println("i : " + i + ", buy1: " + buy1 + ", sell1: " + sell1 + ", buy2 : " + buy2 + ", sell2 : " + sell2);
         }
 
         return sell2;
---
+++ src/LC188MaxProfit.java
@@ -0,0 +1,11 @@
+public class LC188MaxProfit {
+    /**
+     * 也是动态规划问题
+     * @param k
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int k, int[] prices) {
+
+    }
+}
