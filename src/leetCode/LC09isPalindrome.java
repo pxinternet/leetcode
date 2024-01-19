116d134e9803b95d38c46601bf156965a166a4b8
lv <nechina2020@gmail.com>
2024-01-19 14:30:28 +0800

    1

---
+++ src/leetCode/LC09isPalindrome.java
@@ -0,0 +1,23 @@
+package leetCode;
+
+public class LC09isPalindrome {
+
+    public boolean isPalindrome(int x) {
+        if (x == 0) return true;
+
+        //负数和0结尾肯定不是回文数
+        if (x < 0 || x % 10 == 0) return false;
+
+        int reverseNum = 0;
+        while(x > reverseNum) {
+            reverseNum = reverseNum * 10 + x % 10;
+
+            if (x == reverseNum) return true;
+            x /= 10;
+        }
+
+        if (x == reverseNum) return true;
+        return false;
+
+    }
+}
