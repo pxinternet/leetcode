b50946ee45338d95a17b3fcdf19f840089c800d2
lv <nechina2020@gmail.com>
2024-01-30 22:04:21 +0800

    no message

---
+++ src/leetCode/LC105buildTree.java
@@ -0,0 +1,8 @@
+package leetCode;
+
+public class LC105buildTree {
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+    }
+}
--- src/leetCode/TreeNode.java
+++ src/leetCode/TreeNode.java
@@ -6,4 +6,11 @@ public class TreeNode {
    public TreeNode right;
     public TreeNode(int x) { val = x; }
 
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+
+    }
+
 }
