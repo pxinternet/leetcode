267a768bed7009055b21b180dc2e653a1ac7773e
lv <nechina2020@gmail.com>
2024-04-10 18:43:14 +0800

    tree

---
+++ src/leetCode/LC100isSameTree.java
@@ -0,0 +1,10 @@
+package leetCode;
+
+public class LC100isSameTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) return p == q;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
