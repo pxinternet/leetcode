2e64da442aabf77ec4b13b4400cb5e24e83c235c
lv <nechina2020@gmail.com>
2024-04-11 15:45:21 +0800

    1

--- src/leetCode/LC99recoverTree.java
+++ src/leetCode/LC99recoverTree.java
@@ -1,12 +1,40 @@
 package leetCode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class LC99recoverTree {
 
-    TreeNode first = null;
-    TreeNode second = null;
-    TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+//    TreeNode first = null;
+//    TreeNode second = null;
+//    TreeNode prev = new TreeNode(Integer.MIN_VALUE);
     public void recoverTree(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode x = null, y = null, pred = null;
 
+        while(!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (pred != null && root.val < pred.val) {
+                y  = root;
+                if (x == null) {
+                    x = pred;
+                } else {
+                    break;
+                }
+            }
+            pred = root;
+            root = root.right;
+        }
+        swap(x, y);
+    }
 
+    private void swap(TreeNode x, TreeNode y) {
+        int tmp = x.val;
+        x.val = y.val;
+        y.val = tmp;
     }
 }
