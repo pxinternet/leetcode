d6ad144c30c9d3fd8f4cdd4be023547ae59893f5
lv <nechina2020@gmail.com>
2024-04-15 01:06:05 +0800

    1

--- src/leetCode/LC331isValidSerialization.java
+++ src/leetCode/LC331isValidSerialization.java
@@ -2,6 +2,9 @@ package leetCode;
 
 public class LC331isValidSerialization {
 
+    /*
+    要验证二叉树的前序序列化是否有效，我们可以使用一个计数器 degree 来跟踪节点的度数。对于二叉树中的任何节点，如果添加一个新的节点，那么度数就会增加1，因为新的节点提供了一个新的出度；但是如果新的节点是一个非空节点，那么度数就会增加2，因为非空节点提供了两个新的入度。如果序列化是有效的，那么在遍历完整个序列化字符串后，所有的度数应该都能够抵消。
+     */
     public boolean isValidSerialization(String preorder) {
         if (preorder == null || preorder.isEmpty()) return false;
 
