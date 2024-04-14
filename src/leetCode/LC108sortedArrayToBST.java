c46db48985f4823e658ee88fa4390d5f14112684
lv <nechina2020@gmail.com>
2024-04-14 14:28:39 +0800

    1

---
+++ src/leetCode/LC108sortedArrayToBST.java
@@ -0,0 +1,27 @@
+package leetCode;
+
+public class LC108sortedArrayToBST{
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+
+        return buildBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildBST(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        int mid = left + (right- left) / 2;
+
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = buildBST(nums, left, mid -1);
+        node.right = buildBST(nums, mid + 1, right);
+
+        return node;
+    }
+}
---
+++ src/leetCode/LC110isBalanced.java
@@ -0,0 +1,22 @@
+package leetCode;
+
+public class LC110isBalanced {
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        return Math.abs(leftHeight - rightHeight) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return Math.max(height(node.left), height(node.right)) + 1;
+    }
+}
