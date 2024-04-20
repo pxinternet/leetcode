0688ae30cad8a34e574982c9ba411d0a9f83ad8e
lv <nechina2020@gmail.com>
2024-04-20 16:57:48 +0800

    dp

--- src/leetCode/LC188MaxProfit.java
+++ src/leetCode/LC188MaxProfit.java
@@ -8,6 +8,29 @@ public class LC188MaxProfit {
      * @return
      */
     public int maxProfit(int k, int[] prices) {
-        return 0;
+        if (prices.length == 0) {
+            return 0;
+        }
+        if ( k >= prices.length / 2) {
+            int maxProfit = 0;
+            for (int i = 1; i < prices.length; i++) {
+                if (prices[i] > prices[i - 1]) {
+                    maxProfit += prices[i] - prices[i - 1];
+                }
+            }
+            return maxProfit;
+        }
+
+
+        //这个dp要消化一下
+        int[][] dp = new int[k + 1][prices.length];
+        for(int i = 1; i <= k; i++) {
+            int localMax = dp[i - 1][0] - prices[0];
+            for (int j = 1; j < prices.length; j++) {
+                dp[i][j] = Math.max(dp[i][j - 1], prices[j] +localMax);
+                localMax = Math.max(localMax, dp[i - 1][j] - prices[j]);
+            }
+        }
+        return dp[k][prices.length - 1];
     }
 }
