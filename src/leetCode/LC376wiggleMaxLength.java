f2ea6bce6a604adc11fb9eadc1a446f5c4bd54fd
lv <nechina2020@gmail.com>
2024-04-22 20:51:35 +0800

    greedy

---
+++ src/leetCode/LC376wiggleMaxLength.java
@@ -0,0 +1,49 @@
+package leetCode;
+
+public class LC376wiggleMaxLength {
+
+    //动态规划算法
+    public int wiggleMaxLength(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return n;
+        }
+        int[] up = new int[n];
+        int[] down = new int[n];
+
+        up[0] = down[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i-1]) {
+                up[i] = Math.max(up[i-1], down[i-1] + 1);
+                down[i] = down[i-1];
+            } else if (nums[i] < nums[i - 1]) {
+                up[i] = up[i - 1];
+                down[i] = Math.max(up[i - 1] + 1, down[i - 1]);
+            }
+             else {
+                up[i] = up[i-1];
+                down[i] = down[i-1];
+             }
+        }
+        return Math.max(up[n-1], down[n-1]);
+    }
+
+    public int wiggleMaxLengthGreedy(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return n;
+        }
+
+        int prediff = nums[1] - nums[0];
+        int ret = prediff == 0 ? 1 : 2;
+        for (int i = 2; i < n; i++) {
+            int curdiff = nums[i] - nums[i - 1];
+            if ( (prediff <= 0 && curdiff > 0) || (curdiff < 0 && prediff >= 0)) {
+                ret++;
+                prediff = curdiff;
+            }
+        }
+        return ret;
+    }
+}
