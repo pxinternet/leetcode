87a1cbd6db37e8d57f1a4e8a3f2a941f78d07b34
lv <nechina2020@gmail.com>
2024-05-01 00:03:45 +0800

    阿拉伯数字转汉字

---
+++ src/interview/RMBtoHanzi.java
@@ -0,0 +1,63 @@
+package interview;
+
+public class RMBtoHanzi {
+
+    private static final String[] CN_NUMS = {"零","壹","贰","叁","肆","伍","陆","柒","捌","玖"};
+
+    private static final String[] CN_UNITS = {"","拾","佰","仟","万","拾","佰","仟","亿","拾","佰","仟"};
+    private static final String[] CN_DECIMAL_UNIT = {"角", "分"};
+
+    public static String convert(int num) {
+        StringBuilder result = new StringBuilder();
+        String numStr = String.valueOf(num);
+        int length = numStr.length();
+        for (int i = 0; i < length; i++) {
+            int n = numStr.charAt(i) - '0';
+            String cnNum = CN_NUMS[n];
+            String cnUnit = CN_UNITS[length - 1 - i];
+            result.append(cnNum).append(cnUnit);
+        }
+        return result.toString();
+
+    }
+
+
+    public static String convert(double num) {
+        StringBuilder res = new StringBuilder();
+
+        int integerPart = (int)num;
+        int decimalPart = (int)((num - integerPart) * 100);
+
+        String integerPartStr = String.valueOf(integerPart);
+        int length = integerPartStr.length();
+        for (int i = 0; i < length; i++) {
+            int n = integerPartStr.charAt(i) - '0';
+            String cnNum = CN_NUMS[n];
+            String cnUnit = CN_UNITS[length - 1 - i];
+            res.append(cnNum).append(cnUnit);
+        }
+
+        if (decimalPart > 0) {
+            res.append("点");
+            String decimalPartStr = String.format("%02d", decimalPart);
+
+            length = decimalPartStr.length();
+
+            for (int i = 0; i < length; i++) {
+                int n = decimalPartStr.charAt(i) - '0';
+                String cnNum = CN_NUMS[n];
+                String cnUnit = CN_DECIMAL_UNIT[i];
+                res.append(cnNum).append(cnUnit);
+            }
+
+        }
+
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        String res = convert(123456789);
+        System.out.println(res);
+    }
+
+}
--- src/interview/TTLLRU.java
+++ src/interview/TTLLRU.java
@@ -10,131 +10,131 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class TTLLRU {
-
-    //我们通过双向链表的方式来实现一个LRUcache
-
-    class Node {
-        String key;
-        String value;
-
-        long expireTime;
-
-        Node pre;
-        Node next;
-
-        public Node(String key, String value, long expiryTime) {
-            this.key = key;
-            this.value = value;
-            this.expireTime = expiryTime;
-        }
-    }
-
-    class DoublyLinkedList {
-        private Node head;
-        private Node tail;
-
-        public DoublyLinkedList() {
-            head = new Node("", "", 0);
-            tail = new Node("", "", 0);
-            head.next = tail;
-            tail.pre = head;
-        }
-
-        void addHead(Node node) {
-            node.next = head.next;
-            node.pre = head;
-            head.next.pre = node;
-            head.next = node;
-        }
-
-        void remove(Node node) {
-            node.pre.next = node.next;
-            node.next.pre = node.pre;
-        }
-
-        void moveToHead(Node node) {
-            remove(node);
-            addHead(node);
-        }
-
-        Node removeTail() {
-            if (tail.pre == head) {
-                return null;
-            }
-            Node node = tail.pre;
-            remove(node);
-            return node;
-        }
-
-
-    }
-
-    private final Map<String, Node> cache;
-    private final DoublyLinkedList list;
-    private final int capacity;
-
-    private final ScheduledExecutorService executor;
-
-    //还可以进一步优化，用优先级队列；按照时间排序，这样定时任务在遍历的时候，可以提前结束
-
-
-
-    public TTLLRU(int capacity) {
-        this.capacity = capacity;
-        this.cache = new HashMap<>();
-        this.list = new DoublyLinkedList();
-        this.executor = Executor.newScheduledThreadPool(1);
-        startExpirationTask();
-    }
-
-    private void startExpirationTask() {
-        executor.scheduleAtFixedRate(this::expireEntries, 0, 1, TimeUnit.SECONDS);
-    }
-
-    private synchronized  void expireEntries() {
-        long currentTime = System.currentTimeMillis();
-
-        for(Node node : cache.values()) {
-            if (node.expireTime < currentTime) {
-                cache.remove(node);
-                list.remove(node);
-            }
-        }
-    }
-
-    public String get(String key) {
-        if (cache.containsKey(key)) {
-            Node node = cache.get(key);
-            if (node.expireTime < System.currentTimeMillis()) {
-                cache.remove(key);
-                list.remove(node);
-                return null;
-            }
-            list.moveToHead(node);
-            return node.value;
-        }
-        return null;
-    }
-
-    public void put(String key, String value, long lifeTime) {
-        long expireTime = System.currentTimeMillis() + lifeTime;
-
-        if (cache.containsKey(key)) {
-            Node node = cache.get(key);
-            node.value = value;
-            node.expireTime = expireTime;
-            list.moveToHead(node);
-        } else {
-            if (cache.size() == capacity) {
-                Node tail = list.removeTail();
-                cache.remove(tail.key);
-            }
-
-            Node node = new Node(key, value, expireTime);
-            list.addHead(node);
-            cache.put(node.key, node);
-        }
-    }
-
+//
+//    //我们通过双向链表的方式来实现一个LRUcache
+//
+//    class Node {
+//        String key;
+//        String value;
+//
+//        long expireTime;
+//
+//        Node pre;
+//        Node next;
+//
+//        public Node(String key, String value, long expiryTime) {
+//            this.key = key;
+//            this.value = value;
+//            this.expireTime = expiryTime;
+//        }
+//    }
+//
+//    class DoublyLinkedList {
+//        private Node head;
+//        private Node tail;
+//
+//        public DoublyLinkedList() {
+//            head = new Node("", "", 0);
+//            tail = new Node("", "", 0);
+//            head.next = tail;
+//            tail.pre = head;
+//        }
+//
+//        void addHead(Node node) {
+//            node.next = head.next;
+//            node.pre = head;
+//            head.next.pre = node;
+//            head.next = node;
+//        }
+//
+//        void remove(Node node) {
+//            node.pre.next = node.next;
+//            node.next.pre = node.pre;
+//        }
+//
+//        void moveToHead(Node node) {
+//            remove(node);
+//            addHead(node);
+//        }
+//
+//        Node removeTail() {
+//            if (tail.pre == head) {
+//                return null;
+//            }
+//            Node node = tail.pre;
+//            remove(node);
+//            return node;
+//        }
+//
+//
+//    }
+//
+//    private final Map<String, Node> cache;
+//    private final DoublyLinkedList list;
+//    private final int capacity;
+//
+//    private final ScheduledExecutorService executor;
+//
+//    //还可以进一步优化，用优先级队列；按照时间排序，这样定时任务在遍历的时候，可以提前结束
+//
+//
+//
+//    public TTLLRU(int capacity) {
+//        this.capacity = capacity;
+//        this.cache = new HashMap<>();
+//        this.list = new DoublyLinkedList();
+//        this.executor = Executor.newScheduledThreadPool(1);
+//        startExpirationTask();
+//    }
+//
+//    private void startExpirationTask() {
+//        executor.scheduleAtFixedRate(this::expireEntries, 0, 1, TimeUnit.SECONDS);
+//    }
+//
+//    private synchronized  void expireEntries() {
+//        long currentTime = System.currentTimeMillis();
+//
+//        for(Node node : cache.values()) {
+//            if (node.expireTime < currentTime) {
+//                cache.remove(node);
+//                list.remove(node);
+//            }
+//        }
+//    }
+//
+//    public String get(String key) {
+//        if (cache.containsKey(key)) {
+//            Node node = cache.get(key);
+//            if (node.expireTime < System.currentTimeMillis()) {
+//                cache.remove(key);
+//                list.remove(node);
+//                return null;
+//            }
+//            list.moveToHead(node);
+//            return node.value;
+//        }
+//        return null;
+//    }
+//
+//    public void put(String key, String value, long lifeTime) {
+//        long expireTime = System.currentTimeMillis() + lifeTime;
+//
+//        if (cache.containsKey(key)) {
+//            Node node = cache.get(key);
+//            node.value = value;
+//            node.expireTime = expireTime;
+//            list.moveToHead(node);
+//        } else {
+//            if (cache.size() == capacity) {
+//                Node tail = list.removeTail();
+//                cache.remove(tail.key);
+//            }
+//
+//            Node node = new Node(key, value, expireTime);
+//            list.addHead(node);
+//            cache.put(node.key, node);
+//        }
+//    }
+//
 
 }
