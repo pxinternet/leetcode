01785f7bbe8b66a53565ec002fb2dea96ebcd362
lv <nechina2020@gmail.com>
2024-07-05 14:00:08 +0800

    239

---
+++ src/leetCode/LC239.java
@@ -0,0 +1,36 @@
+﻿package leetCode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class LC239 {
+
+    public int[] maxSlidingWindoe(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k <= 0) {
+            return new int[0];
+        }
+
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i =0; i < n; i++) {
+            while (!deque.isEmpty() && deque.peek() < i - k + 1) {
+                deque.poll();
+            }
+
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+            deque.offer(i);
+
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[deque.peek()];
+            }
+        }
+        return result;
+
+    }
+
+}
