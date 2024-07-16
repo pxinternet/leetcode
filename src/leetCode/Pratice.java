62a6ed690a7b08747fc6cad173a5d59a2852a18c
lv <nechina2020@gmail.com>
2024-07-16 22:59:37 +0800

    practice

--- src/leetCode/Pratice.java
+++ src/leetCode/Pratice.java
@@ -2689,6 +2689,129 @@ public class Pratice {
     }
 
 
+    public int uniquePathsWithObstacles( int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                break;
+            }
+            dp[i][0] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (obstacleGrid[0][i] == 1) {
+                break;
+            }
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    public String longestPalindrome(String s) {
+
+        int n = s.length();
+
+        int start = 0;
+        int end = 0;
+
+        int maxLength = 0;
+
+        for (int i = 0; i < n; i++) {
+            int len1 = expendFromCenter(s, i, i);
+            int len2 = expandFromCenter(s, i, i + 1);
+
+            int len = Math.max(len1, len2);
+
+            if (maxLength < len) {
+                maxLength = len;
+                start = i - (len - 1) / 2;
+                end = i + len /2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+
+    }
+
+    private int expandFromCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    public int minDistance(String word1, String word2) {
+
+        int m = word1.length();
+        int n = word2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        dp[0][0] = 0;
+
+        for (int i = 1; i < m + 1; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    public int maximalSquare(char[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[][] dp = new int[m][n];
+
+        int maxSquare = Integer.MIN_VALUE;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '0') {
+                    dp[i][j] = 0;
+                } else {
+                    if (i == 0 && j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    }
+                }
+
+                maxSquare = Math.max(maxSquare, dp[i][j]);
+            }
+        }
+
+        return maxSquare * maxSquare；
+    }
+
+
 }
 
 
