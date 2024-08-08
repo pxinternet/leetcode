961bbff2b05e602326a2a50faa6f84302a55f0bb
lv <nechina2020@gmail.com>
2024-08-09 00:36:10 +0800

    1

---
+++ src/dp/LC1143longestCommonSubswquence.java
@@ -0,0 +1,33 @@
+﻿package dp;
+
+public class LC1143longestCommonSubswquence {
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+
+        dp[0][0] = 0;
+
+        int len1 = text1.length();
+        int len2 = text2.length();
+
+        for (int i = 1; i <= len1; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int j = 1; j <= len2; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+
+}
---
+++ src/dp/LC354maxEnvelopes.java
@@ -0,0 +1,79 @@
+﻿package dp;
+
+import java.util.Arrays;
+
+import java.util.Arrays;
+
+import java.util.Arrays;
+
+public class LC354maxEnvelopes {
+
+    public int maxEnvelopes(int[][] envelopes) {
+
+        if (envelopes == null || envelopes.length == 0 || envelopes[0].length != 2) {
+            return 0;
+        }
+
+        //step 1 Sort the envelops
+
+        Arrays.sort(envelopes, (a, b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - a[1];
+            } else {
+                return a[0] - b[0];
+            }
+        });
+
+        //Step2 Find the longest increasing subsequence based on heights
+        int[] dp = new int[envelopes.length];
+        int length = 0;
+
+        for (int[] envelope : envelopes) {
+            int height = envelope[1];
+            int index = Arrays.binarySearch(dp, 0, length, height);
+            if (index < 0) {
+                index = -(index + 1);
+            }
+            dp[index] = height;
+            if (index == length) {
+                length++;
+            }
+        }
+        return length;
+
+    }
+
+    public int maxEnvelopes2(int[][] envelops) {
+        if (envelops.length == 0) {
+            return 0;
+        }
+
+        int n = envelops.length;
+
+        Arrays.sort(envelops, (a, b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - a[1];
+            } else {
+                return a[0] - b[0];
+            }
+        });
+
+        int[] dp = new int[n];
+
+        Arrays.fill(dp, 1);
+
+        int ans  = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (envelops[j][1] < envelops[i][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+
+
+}
